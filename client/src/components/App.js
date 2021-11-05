2f8bd97bdabf6b6275922695d5c33f5f615ffe5a
Katie Eagles <kteagles21@gmail.com>
2021-11-04 21:34:22 -0400

    updated queries

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import '../css/app.css'
+import CourseData from './CourseData'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
     return (
+        <ApolloProvider client={client}>
+        <>
         <div>Hello World! This is project 3!</div>
+        <CourseData />
+        </>
+        </ApolloProvider>
     )
 }
 
