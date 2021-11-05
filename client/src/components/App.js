cce7fdb41e591618e3f03887ec62afcbfaf18319
chemacenturion <centurion.philadelphia@gmail.com>
2021-11-04 21:40:11 -0400

    pushing up Navbar with links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 import '../css/app.css'
+
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Navbar from './Navbar';
+
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
 
 function App() {
     return (
-        <div>Hello World! This is project 3!</div>
+        <ApolloProvider client={client}>
+        <Router>
+            <Navbar />
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/Login' component={Login}/>
+            <Route exact path='/Register' component={Register}/>
+        </Router>
+        </ApolloProvider>
     )
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Register.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'semantic-ui-react';
+import { gql, useMutation } from '@apollo/client';
+
+const Register = () => {
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    const onChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value});
+    }
+
+    const [addUser, { loading }] = useMutation(ADD_USER, {
+        update(proxy, result){
+            console.log(result)
+        },
+        variables: values
+    })
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addUser()
+    }
+
+    return (
+        <div>
+            <Form onSubmit={onSubmit} noValidate>
+                <h1>Register</h1>
+                <Form.Input
+                    label="Username"
+                    placeholder="Username.."
+                    name="username"
+                    type="text"
+                    value={values.username}
+                    onChange={onChange}
+                    />
+                <Form.Input
+                    label="Email"
+                    placeholder="Email.."
+                    name="email"
+                    type="email"
+                    value={values.email}
+                    onChange={onChange}
+                    />
+                <Form.Input
+                    label="Password"
+                    placeholder="Password.."
+                    name="password"
+                    type="password"
+                    value={values.password}
+                    onChange={onChange}
+                    />
+                    <Button type="submit" secondary>
+                        Register
+                    </Button>
+            </Form>
+        </div>
+    )
+}
+
+const ADD_USER = gql`
+mutation addUser(
+  $username: String!, 
+  $email: String!, 
+  $password: String!
+  ) {
+  addUser(
+    username: $username, 
+    email: $email, 
+    password: $password
+    ) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
+`;
+
+export default Register;
