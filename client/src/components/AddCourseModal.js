ad3f665bca332783e90dd97cd79f453523cd76e6
chemacenturion <centurion.philadelphia@gmail.com>
2021-11-10 20:58:39 -0500

    added course modal

---
+++ client/src/components/AddCourseModal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Modal, Button } from 'react-bootstrap'
+import AddCourseData from './AddCourseData';
+
+export default function AddCourseModal(props) {
+
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Modal heading
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <AddCourseData />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
--- client/src/pages/Scorecard.js
+++ client/src/pages/Scorecard.js
@@ -2,8 +2,12 @@ import React from 'react'
 import CourseData from '../components/CourseData'
 import AddCourseData from '../components/AddCourseData'
 import { Container, Button } from 'react-bootstrap'
+import AddCourseModal from '../components/AddCourseModal'
 
 const Scorecard = () => {
+
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
         <Container className="mt-5 scoring__table">
             <h1>Welcome to the Scoring Table</h1>
@@ -13,7 +17,7 @@ const Scorecard = () => {
                 </div>
                 <div className="scoring__table-buttons">
                     <div className="d-grid gap-2">
-                        <Button variant="primary" size="lg">
+                        <Button variant="primary" size="lg" onClick={() => setModalShow(true)}>
                             Add Course to Database
                         </Button>
                         <Button variant="success" size="lg">
@@ -21,7 +25,11 @@ const Scorecard = () => {
                         </Button>
                     </div>
                 </div>
-            <AddCourseData />
+            
+            <AddCourseModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
         </Container>
     )
 }
